9dbab5dc4e61b2ff334cb1e0df02968885e380b8
David Mansson <masenius@gmail.com>
2024-09-25 16:51:38 +0200

    feat(theme): switch to catppuccin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -32,8 +32,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  -- install = { colorscheme = { "onedark", "tokyonight", "habamax" } },
-  install = { colorscheme = { "onedark" } },
+  install = { colorschema = { "catppuccin" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
