1069eb52d258352ebec822eab45849fc4d29a54e
David Mansson <masenius@gmail.com>
2024-09-25 16:51:38 +0200

    switch to catppuccin

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,3 +12,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.opt.spelllang = { "en", "sv" }
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  { "joshdick/onedark.vim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
     },
   },
 }
