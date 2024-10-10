cb691fb7d4dc5aed094178b3aaf4c96704886531
David Mansson <david.mansson@hm.com>
2024-10-10 17:58:25 +0200

    cleanup bashrc

---
+++ .bashrc.d/main.sh
@@ -0,0 +1,14 @@
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export KUBE_CONFIG_PATH=$HOME/.kube/config
+export EDITOR=nvim
+
+source $HOME/.cargo/env
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(starship init bash)"
