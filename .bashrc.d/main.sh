2a5f1ac5395fc0cb9b5fa44a57e195828b1a3b16
David Mansson <masenius@gmail.com>
2025-01-04 14:29:22 +0100

    add cargo env

--- .bashrc.d/main.sh
+++ .bashrc.d/main.sh
@@ -6,7 +6,9 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+. "$HOME/.cargo/env"
 
 eval "$(starship init bash)"
