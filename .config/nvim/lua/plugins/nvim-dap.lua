bbdd13dea4baba13a300223d67f27c618ffc48d7
David Mansson <masenius@gmail.com>
2025-08-13 18:23:54 +0200

    feat(nvim): add new plugins

---
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,40 @@
+return {
+  "mfussenegger/nvim-dap",
+  keys = {
+    {
+      "<F9>",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Run/Continue",
+    },
+    {
+      "<leader>dC",
+      function()
+        require("dap").run_to_cursor()
+      end,
+      desc = "Run to Cursor",
+    },
+    {
+      "<F7>",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<F6>",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+    {
+      "<F8>",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+  },
+}
