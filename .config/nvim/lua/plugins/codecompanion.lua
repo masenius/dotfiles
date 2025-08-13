853ee4b98aa2c38996f16ff4717340f3a3af4fa0
David Mansson <masenius@gmail.com>
2025-08-13 09:28:38 +0200

    feat(nvim): add copilot

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -9,8 +9,25 @@ return {
     --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
     strategies = {
       --NOTE: Change the adapter as required
-      chat = { adapter = "ollama" },
-      inline = { adapter = "ollama" },
+      chat = { adapter = "copilot" },
+      inline = { adapter = "copilot" },
+      cmd = { adapter = "copilot" },
+      -- chat = { adapter = "ollama" },
+      -- inline = { adapter = "ollama" },
+    },
+  },
+  extensions = {
+    mcphub = {
+      callback = "mcphub.extensions.codecompanion",
+      opts = {
+        make_tools = true,
+        show_server_tools_in_chat = true,
+        add_mcp_prefix_to_tool_names = true,
+        show_result_in_chat = true,
+        format_tool = nil,
+        make_vars = true,
+        make_slash_commands = true,
+      },
     },
   },
 }
