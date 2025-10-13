2a987e00bfd42a31b5c809d3b6d6c0548aa420b8
David Mansson <masenius@gmail.com>
2025-10-13 09:52:31 +0200

    remove mcphub

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -15,19 +15,5 @@ return {
       -- chat = { adapter = "ollama" },
       -- inline = { adapter = "ollama" },
     },
-    extensions = {
-      mcphub = {
-        callback = "mcphub.extensions.codecompanion",
-        opts = {
-          make_tools = true,
-          show_server_tools_in_chat = true,
-          add_mcp_prefix_to_tool_names = true,
-          show_result_in_chat = true,
-          format_tool = nil,
-          make_vars = true,
-          make_slash_commands = true,
-        },
-      },
-    },
   },
 }
